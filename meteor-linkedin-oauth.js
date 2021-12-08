bba4ea1779e5ec091ffe74941c8ad8983a1b6541
Juan Pablo Mejia <juanpmd@hotmail.com>
2021-12-08 13:59:17 -0500

    feat(general): add initial package files

---
+++ meteor-linkedin-oauth.js
@@ -0,0 +1,5 @@
+// Write your package code here!
+
+// Variables exported by this module can be imported by other packages and
+// applications. See meteor-linkedin-oauth-tests.js for an example of importing.
+export const name = 'meteor-linkedin-oauth';
