38c82b6a9e19e1bdd2b39b7c859481491ae5c7b1
Juan Pablo Mejia <juanpmd@hotmail.com>
2022-01-09 18:43:55 -0500

    Bump version to 1.0.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'codifytools:linkedin-oauth',
-  version: '1.0.3',
+  version: '1.0.4',
   summary: 'Linkedin OAuth flow',
   git: 'https://github.com/codifytools/meteor-linkedin-oauth',
   documentation: null
